84685fca29b9f906f1b2f4e636d83dc67081f2b4
Manoj Kumar <113518119+royalmstar007@users.noreply.github.com>
2023-02-24 09:28:21 +0530

    Delete ToDo_Sample directory

--- ToDo_Sample/config.php
+++
@@ -1,4 +0,0 @@
-<?php
-    $conn = mysqli_connect("localhost","root","","crud") or die("connection fail");
-    
-?>
--- ToDo_Sample/delete.php
+++
@@ -1,7 +0,0 @@
-<?php
-include "config.php";
-echo $id=$_GET['ID'];
-mysqli_query($conn,"delete from todo where id=$id");
-header("location:index.php");
-
-?>
